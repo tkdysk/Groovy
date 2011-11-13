89bbecdc26d3345f4020efafd5f03bfbd122f775
tkdysk <tkdysk@gmail.com>
2011-11-13 11:11:56 +0900

    MakeThumbnailをコミット

---
+++ 2011/MakeThumbnail/MakeThumbnail.groovy
@@ -0,0 +1,64 @@
+/**
+* thumbnailator - Thumbnailator - a thumbnail generation library for Java - Google Project Hosting
+* http://code.google.com/p/thumbnailator/
+* Thumbnailator-0.3.10.jarを、~/.groovy/lib か <GROOVY_HOME>/lib にコピー
+*/
+
+import net.coobird.thumbnailator.Thumbnails
+import net.coobird.thumbnailator.resizers.configurations.Antialiasing
+
+/** Config */
+File rootDir = new File(".")
+File pictureDir = new File("./pictures")
+File thumbnailDir = new File("./thumbnail")
+
+/** initialize */
+if(!pictureDir.exists()) pictureDir.mkdir()
+if(thumbnailDir.exists()) thumbnailDir.deleteDir()
+thumbnailDir.mkdir()
+
+/** Get Jpeg pictures Path */
+List pictures = findPictures(pictureDir, ~/.*\.[jJ][pP][gG]/)
+
+/** Create Thumbnails */
+List thumbnails = createThumbnails(pictureDir, thumbnailDir, pictures, 800)
+
+/** Completed! Message */
+println "Completed!"
+
+
+/** function - findPictures  */
+def findPictures(File rootDir, java.util.regex.Pattern filter) {
+  List pictures = new ArrayList()
+  rootDir.traverse(
+    type         : groovy.io.FileType.FILES,
+    nameFilter   : filter
+  ) { 
+      pictures.add( it )
+  }
+  return pictures
+}
+
+/** function - createThumbnails */
+def createThumbnails(File pictureDir, File thumbnailDir, List pictures, int width) {
+  List thumbnails = new ArrayList()
+  for(File picture : pictures){
+    String relativePath = picture.canonicalPath - pictureDir.canonicalPath
+    File thumbnail = new File(thumbnailDir.canonicalPath + relativePath)
+    File parentDir = new File(thumbnail.parent)
+    if (!parentDir.exists()) parentDir.mkdirs()
+    
+    Thumbnails.of(picture)
+      .width(width)
+      .keepAspectRatio(true)
+      .antialiasing(Antialiasing.ON)
+      .outputQuality(1.0d)
+      .allowOverwrite(true)
+      .toFile(thumbnail)
+    
+    thumbnails.add(thumbnail)
+  }
+  return thumbnails
+}
+
+
